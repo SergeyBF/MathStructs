6bea44b87d598efa47393a734eff8b868a8e40b5
SergeyBF <bsergeyf@gmail.com>
2015-09-11 13:26:50 +0200

    addig files

---
+++ LinearSpace/LinearSpace/LinearSpace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SergeyMS
+{
+    /// <summary>
+    /// Linear Space interface
+    /// <remarks>
+    /// This contain the criterias of mathematical axiomas for Linear Space.
+    /// </remarks>
+    /// </summary>
+    /// <typeparam name="T"> This type param is only for real number typed variables. </typeparam>
+    public interface LinearSpace<T> where T : IConvertible
+    {
+        LinearSpace<T> Operation(LinearSpace<T> Element1, LinearSpace<T> Element2);
+        LinearSpace<T> Operation_inverse(LinearSpace<T> Element1, LinearSpace<T> Element2);
+        LinearSpace<T> MultiplyScarar(T Scalar, LinearSpace<T> Element);
+        LinearSpace<T> MultiplyScarar(LinearSpace<T> Element, T Scalar);
+        bool Equals(LinearSpace<T> Element1, LinearSpace<T> Element2);
+        int GetHashCode();
+        LinearSpace<T> Neutral_element { get; }
+    }
+}
---
+++ LinearSpace/LinearSpace/RealMatrix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SergeyMS
+{
+    /// <summary>
+    /// Class of general Matrix
+    /// <remarks>
+    /// 
+    /// </remarks>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class RealMatrix<T> : LinearSpace<T> where T : IConvertible
+    {
+        /// <summary>
+        /// Data members:
+        /// <param name="rows"> Number of matrix rows.</param>
+        /// <param name="cols"> Number of matrix coloums.</param>
+        /// <param name="array"> An array for store elements of matrix.</param>
+        /// </summary>
+        protected ulong row;
+        protected ulong cols;
+        protected T[] array;
+
+    }
+}

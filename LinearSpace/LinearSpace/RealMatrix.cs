93014dc18fdb83b4110198eaa86ebe42b229aa16
SergeyBF <bsergeyf@gmail.com>
2015-09-13 16:42:32 +0200

    na03

--- LinearSpace/LinearSpace/RealMatrix.cs
+++ LinearSpace/LinearSpace/RealMatrix.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SergeyMS
 {
     /// <summary>
-    /// vv
+    /// vvx
     /// Class of general Matrix
     /// <remarks>
     /// This class work with value types only.
